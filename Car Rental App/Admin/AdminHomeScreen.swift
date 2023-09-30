dad2a1c0304b1fb933024118459278990697ece3
Xuananhnguyen <xuananhnguyen0306@gmail.com>
2023-09-30 15:34:09 +0700

    Create file User and Admin

---
+++ Car Rental App/Admin/AdminHomeScreen.swift
@@ -0,0 +1,16 @@
+//
+//  AdminHomeScreen.swift
+//  Car Rental App
+//
+//  Created by NGUYEN XUAN ANH on 30/09/2023.
+//
+
+import SwiftUI
+
+struct AdminHomeScreen: AppNavigator {
+    var body: some View {
+        VStack {
+            Text("Xin chào, Admin!")
+        }
+    }
+}
